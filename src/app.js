333021eac720607c0c1f312036bdc20d8e600747
Daniel Rodriguez <127802925+danielrodc@users.noreply.github.com>
2023-05-18 17:35:24 +0000

    daniels fixes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
 let pronoun = ["her", "his", "their", "our", "my"];
 let adj = ["pretty", "hairy", "small", "great", "bad"];
 let noun = ["book", "leg", "table", "box", "cat", "dog"];
 let dom = [".com", ".org", ".co", ".gov", ".edu"];
 
 for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.lenght; j++) {
-    {
-      for (let k = 0; k < noun.lenght; j++) {
-        {
-          for (let m = 0; m < dom.lenght; j++) {
-            // eslint-disable-next-line no-console
-            console.log("${pronoun[i]}${adj[j]}${noun[k]}${dom[m]}");
-          }
-        }
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < dom.length; m++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}${dom[m]}`);
       }
     }
   }
